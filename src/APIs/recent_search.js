d81727d40b074dc01ddad19055112a9f239a691f
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2023-03-12 11:07:28 -0400

    poll from twitter again

--- src/APIs/recent_search.js
+++ src/APIs/recent_search.js
@@ -6,8 +6,11 @@ import axios from "axios";
 // The code below sets the bearer token from your environment variables
 // To set environment variables on macOS or Linux, run the export command below from the terminal:
 // export BEARER_TOKEN='YOUR-TOKEN'
-
-const endpointUrl = ""; // FIXME: comment me back
+console.log("NODE_ENV", process.env.NODE_ENV);
+const endpointUrl =
+  process.env.NODE_ENV === "development"
+    ? ""
+    : "https://j9xb399hhb.us-east-1.awsapprunner.com"; // FIXME: comment me back
 // const endpointUrl = "";
 
 const instance = axios.create({
