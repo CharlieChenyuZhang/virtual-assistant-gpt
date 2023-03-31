3e8961498c7a63489718add2641d69bb94c29f9b
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2023-03-30 23:18:11 -0400

    mobius static page working

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -11,7 +11,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import AllInclusiveIcon from "@mui/icons-material/AllInclusive";
 
 const pages = ["Products", "Pricing", "Blog"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -36,10 +36,12 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ background: "black" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <AllInclusiveIcon
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+          />
           <Typography
             variant="h6"
             noWrap
@@ -55,7 +57,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            MOBIUS
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -94,7 +96,9 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          {/* <AllInclusiveIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
           <Typography
             variant="h5"
             noWrap
@@ -111,8 +115,8 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            LOGO
-          </Typography>
+            MOBIUS
+          </Typography> */}
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
--- src/components/TextToImage.js
+++ src/components/TextToImage.js
@@ -13,7 +13,17 @@ import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import styled from "styled-components";
 import photos from "./photos";
-import ResponsiveApBar from './ResponsiveAppBar';
+import ResponsiveApBar from "./ResponsiveAppBar";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
 
 const slides = photos.map(({ src, width, height, images }) => ({
   src,
@@ -30,12 +40,78 @@ const MainContainer = styled.div`
   background-color: #27272a;
 `;
 
+const H1 = styled.h1`
+  font-size: 3.75rem;
+  line-height: 1;
+  font-weight: 700;
+  color: #fff;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export default function TextToImage() {
   const [index, setIndex] = useState(-1);
 
   return (
     <MainContainer>
-    <ResponsiveApBar></ResponsiveApBar>
+      <ResponsiveApBar></ResponsiveApBar>
+      <HeaderContainer>
+        <Typography
+          variant="h1"
+          fontSize="5.2rem"
+          marginTop="64px"
+          noWrap
+          component="div"
+          sx={{
+            fontFamily: "Roboto",
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          MOBIUS
+        </Typography>
+        <Typography
+          variant="h5"
+          fontSize="1.2rem"
+          noWrap
+          component="div"
+          sx={{
+            mr: 2,
+            fontFamily: "Roboto",
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          unprecedented photorealism x deep level of language understanding
+        </Typography>
+
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: "80%",
+            maxWidth: "600px",
+            borderRadius: "100px",
+            marginTop: "32px",
+            marginBottom: "64px",
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="A cute corgi lives in a house of sushi..."
+            inputProps={{ "aria-label": "search" }}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </HeaderContainer>
       <PhotoAlbum
         photos={photos}
         layout="rows"
